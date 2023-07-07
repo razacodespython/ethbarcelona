d2a8b302ed737742c635fca3f502ce19a79ea774
razacodespython <srh.zaidi@outlook.com>
2023-07-08 01:08:46 +0200

    connect

--- ethbarcelona/pages/index.js
+++ ethbarcelona/pages/index.js
@@ -1,6 +1,25 @@
-
+import { useState } from 'react';
+import { ethers } from 'ethers';
 
 export default function Home() {
+  const [addi, setPublickey] = useState();
+  const [network, setNetwork] = useState();
+  const [chainId, setChainId] = useState();
+  const [msg, setMsg] = useState();
+
+  const connectButton = async () => {
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    if (ethereum.isMetaMask) {
+      const accounts = await provider.send("eth_requestAccounts", [])
+      const { name, chainId } = await provider.getNetwork();
+      setNetwork(name);
+      setChainId(chainId);
+      setPublickey(accounts[0]);
+    } else {
+      setMsg("Install MetaMask");
+    }
+  };
   const messages = "text"
   
   const options = {
@@ -21,7 +40,12 @@ export default function Home() {
   return (
     <>
      <h1>Hello World</h1>
+     <button onClick={connectButton}>Connect Wallet</button>
      <button  type="submit" onClick={apiCall}> apicall </button>
+     <p>Address: {addi}</p>
+      <p>Network: {network}</p>
+      <p>Chain ID : {chainId}</p>
+      {msg && <p>{msg}</p>}
     </>
   )
 }
