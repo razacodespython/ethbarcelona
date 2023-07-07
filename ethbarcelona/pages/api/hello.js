9e9b7e7d1aa17adda2e1560b552dfe9cb95dd083
razacodespython <srh.zaidi@outlook.com>
2023-07-08 00:52:03 +0200

    setup api

--- ethbarcelona/pages/api/hello.js
+++ ethbarcelona/pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log("message received")
+  const body = req.body;
+  console.log(body)
+  const response={"message":"hi back"}
+  res.send(response)
+
 }
--- ethbarcelona/pages/index.js
+++ ethbarcelona/pages/index.js
@@ -1,14 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const messages = "text"
+  
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(messages),
+  };
+
+  const apiCall = async () => {
+    //goes to folder
+    console.log("clicked");
+    const response = await fetch("/api/hello", options);
+    const data = await response.json();
+    console.log(data)
+  };
   return (
     <>
      <h1>Hello World</h1>
+     <button  type="submit" onClick={apiCall}> apicall </button>
     </>
   )
 }
