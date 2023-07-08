4dbe8715d55972772c05b37748160debcd968596
Orest Tarasiuk <830847+OrestTa@users.noreply.github.com>
2023-07-08 17:41:00 +0200

    simplify Ballot contract, check and store nullifiers; zkp verfication TODO

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -4,138 +4,57 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /** 
  * @title Ballot
- * @dev Implements voting process along with vote delegation
+ * @dev Implements zkVoting process
  */
 contract Ballot {
-
-    struct Voter {
-        uint weight; // weight is accumulated by delegation
-        bool voted;  // if true, that person already voted
-        address delegate; // person delegated to; shouldn't be needed for now
-        uint vote;   // index of the voted proposal
-    }
-
-    struct Proposal {
-        // If you can limit the length to a certain number of bytes, 
-        // always use one of bytes1 to bytes32 because they are much cheaper
-        bytes32 name;   // short name (up to 32 bytes)
-        uint voteCount; // number of accumulated votes
-    }
-
     address public chairperson; // this could be the zkp verifier contract; this contract's deployer
 
-    mapping(address => Voter) public voters;
+    uint public quorumInFavour;
+    uint public votesInFavour = 0;
+    uint public votesAgainst = 0;
+    uint public votesAbstaining = 0;
 
-    Proposal[] public proposals;
+    bytes32[] public nullifiers;
 
     /** 
-     * @dev Create a new ballot to choose one of 'proposalNames'.
-     * @param proposalNames names of proposals
+     * @dev Create a new ballot
+     * @param quorumInFavour_ the minimum number of yes-votes (0) for the ballot to be deemed accepted; 
+     *   can be expanded to support other proposal types (no (1), abstain (2))
      */
-    constructor(bytes32[] memory proposalNames) {
+    constructor(uint quorumInFavour_) {
         chairperson = msg.sender;
-        voters[chairperson].weight = 1;
-
-        for (uint i = 0; i < proposalNames.length; i++) {
-            // 'Proposal({...})' creates a temporary
-            // Proposal object and 'proposals.push(...)'
-            // appends it to the end of 'proposals'.
-            proposals.push(Proposal({
-                name: proposalNames[i],
-                voteCount: 0
-            }));
-        }
-    }
-
-    /** 
-     * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
-     * @param voter address of voter
-     * TODO: Convert to run at init using our MPT entries
-     */
-    function giveRightToVote(address voter) public {
-        require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
-        );
-        // TODO: Use a nullifier check instead
-        require(
-            !voters[voter].voted,
-            "The voter already voted."
-        );
-        require(voters[voter].weight == 0);
-        voters[voter].weight = 1;
+        quorumInFavour = quorumInFavour_;
     }
 
     /**
-     * @dev Delegate your vote to the voter 'to'.
-     * @param to address to which vote is delegated
+     * @dev Give your vote
+     * @param proposalType 0 to vote for yes, 1 for no, 2 for abstain
      */
-    function delegate(address to) public {
-        Voter storage sender = voters[msg.sender];
-        require(!sender.voted, "You already voted.");
-        require(to != msg.sender, "Self-delegation is disallowed.");
+    function vote(uint8 proposalType, bytes32 zkProof, bytes32 nullifier) public {
 
-        while (voters[to].delegate != address(0)) {
-            to = voters[to].delegate;
+        /* TODO: Verify zkProof */
 
-            // We found a loop in the delegation, not allowed.
-            require(to != msg.sender, "Found loop in delegation.");
-        }
-        sender.voted = true;
-        sender.delegate = to;
-        Voter storage delegate_ = voters[to];
-        if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes
-            proposals[delegate_.vote].voteCount += sender.weight;
-        } else {
-            // If the delegate did not vote yet,
-            // add to her weight.
-            delegate_.weight += sender.weight;
-        }
-    }
-
-    /**
-     * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
-     * @param proposal index of proposal in the proposals array
-     * TODO: Convert to ZK - only tally the sum
-     */
-    function vote(uint proposal) public {
-        Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, "Has no right to vote");
-        require(!sender.voted, "Already voted.");
-        sender.voted = true;
-        sender.vote = proposal;
-
-        // If 'proposal' is out of the range of the array,
-        // this will throw automatically and revert all
-        // changes.
-        proposals[proposal].voteCount += sender.weight;
-    }
-
-    /** 
-     * @dev Computes the winning proposal taking all previous votes into account.
-     * @return winningProposal_ index of winning proposal in the proposals array
-     */
-    function winningProposal() public view
-            returns (uint winningProposal_)
-    {
-        uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                winningProposal_ = p;
+        for (uint i=0; i<nullifiers.length; i++) {
+            if (nullifiers[i]==nullifier) {
+                revert("Voter has already voted");
             }
         }
+        nullifiers.push(nullifier);
+
+        if (proposalType==0) {
+            votesInFavour++;
+        }
+        if (proposalType==1) {
+            votesAgainst++;
+        }
+        if (proposalType==2) {
+            votesAbstaining++;
+        }
     }
 
-    /** 
-     * @dev Calls winningProposal() function to get the index of the winner contained in the proposals array and then
-     * @return winnerName_ the name of the winner
-     */
-    function winnerName() public view
-            returns (bytes32 winnerName_)
-    {
-        winnerName_ = proposals[winningProposal()].name;
-    }
+    function ballotWasAccepted() public view 
+        returns (bool accepted) 
+        {
+            return votesInFavour >= quorumInFavour;
+        }
 }
